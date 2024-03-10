33724833a0e2ae745386595ddd52d83f0ce2ae2e
J.P. Hutchins <jphutchins@gmail.com>
2024-03-09 22:36:39 -0800

    wip: minimal fpm

---
+++ distribution/fpm/after-install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Copyright (c) 2024 JP Hutchins
+# SPDX-License-Identifier: Apache-2.0
+
+# add a symlink so that the app can be run from the command line
+ln -s /usr/share/myapp/myapp /usr/bin/myapp
---
+++ distribution/fpm/after-remove.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Copyright (c) 2024 JP Hutchins
+# SPDX-License-Identifier: Apache-2.0
+
+# remove the symlink that was created by after-install.sh
+rm /usr/bin/myapp
